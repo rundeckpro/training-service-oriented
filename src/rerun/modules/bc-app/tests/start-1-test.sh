230f938fd29f7bf72017a3b195caa8c5a29b9138
Alex Honor <alexhonor@yahoo.com>
2013-09-09 17:15:37 -0700

    added skeleton bc-module source (stop, start, status).

---
+++ src/rerun/modules/bc-app/tests/start-1-test.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env roundup
+#
+# This file contains the test plan for the `start` command.
+#    
+#/ usage:  rerun stubbs:test -m bc-app -p start [--answers <>]
+#
+
+# Helpers
+#
+[[ -f ./functions.sh ]] && . ./functions.sh
+
+# The Plan
+# --------
+
+describe "start"
+
+
+# ------------------------------
+# Replace this test. 
+it_fails_without_a_real_test() {
+    exit 1
+}
+# ------------------------------
+
---
+++ src/rerun/modules/bc-app/tests/status-1-test.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env roundup
+#
+# This file contains the test plan for the `status` command.
+#    
+#/ usage:  rerun stubbs:test -m bc-app -p status [--answers <>]
+#
+
+# Helpers
+#
+[[ -f ./functions.sh ]] && . ./functions.sh
+
+# The Plan
+# --------
+
+describe "status"
+
+
+# ------------------------------
+# Replace this test. 
+it_fails_without_a_real_test() {
+    exit 1
+}
+# ------------------------------
+
